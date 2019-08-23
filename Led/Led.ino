ea17cea8a1096399601aa1047d065c4deb600684
Capel001 <willychang008@gmail.com>
2019-08-23 10:31:30 +0800

    Update Led.ino

--- Led/Led.ino
+++ Led/Led.ino
@@ -5,6 +5,19 @@ class Led{
   public:
     Led(byte pin){
       _pinNum = pin;
+      pinMode(_pinNum,OUTPUT);
+     }
+
+     byte getPinNum(){
+        return _pinNum;
+     }
+
+     void on(){
+      digitalWrite(_pinNum,HIGH);
+     }
+
+     void off(){
+      digitalWrite(_pinNum,LOW);
      }
 };
 
@@ -14,8 +27,10 @@ Led secondLed(12);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println( firstLed._pinNum);
-  Serial.println( secondLed._pinNum);
+  Serial.println( firstLed.getPinNum());
+  Serial.println( secondLed.getPinNum());
+  firstLed.on();
+  secondLed.on();
   }
 
 void loop() {
