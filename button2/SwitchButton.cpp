393120173f5ef0a02dbc81973bbee4754ff33d07
Capel001 <willychang008@gmail.com>
2019-08-27 10:20:26 +0800

    up SwitchButton.cpp
    include SwitchButton.cpp to SwitchButton.h

--- button2/SwitchButton.cpp
+++ button2/SwitchButton.cpp
@@ -1,28 +1,21 @@
-#include <arduino.h>
+#include "SwitchButton.h"
 
-class SwitchButton{
-  private:
-    byte _pinNum;
-    unsigned long _stateChangeCount;
-    boolean _state;
 
-  public:
-    SwitchButton(byte pin){
-      _pinNum=pin;
-      _stateChangeCount=0;
-      pinMode(_pinNum,INPUT_PULLUP);
-      _state=digitalRead(_pinNum);  
+SwitchButton::SwitchButton(byte pin) {
+  _pinNum = pin;
+  _stateChangeCount = 0;
+  pinMode(_pinNum, INPUT_PULLUP);
+  _state = digitalRead(_pinNum);
+}
+
+unsigned long SwitchButton::getStateChangeCount() {
+  boolean nowState = digitalRead(_pinNum);
+  if (_state != nowState) {
+    delay(10);
+    if (nowState == digitalRead(_pinNum)) {
+      _state = nowState;
+      _stateChangeCount++;
     }
-
-    unsigned long getStateChangeCount(){
-       boolean nowState = digitalRead(_pinNum);
-       if (_state!=nowState){
-          if (nowState == digitalRead(_pinNum)) {
-           _state = nowState;
-           _stateChangeCount++;
-          } 
-       } 
-    return _stateChangeCount; 
-    }
-  
-};
+  }
+  return _stateChangeCount;
+}
