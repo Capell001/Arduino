cf1c256effd528594ca0f9b63c3f9ed9dd135460
Capel001 <willychang008@gmail.com>
2019-08-27 10:16:06 +0800

    add .cpp

---
+++ button2/SwitchButton.cpp
@@ -0,0 +1,28 @@
+#include <arduino.h>
+
+class SwitchButton{
+  private:
+    byte _pinNum;
+    unsigned long _stateChangeCount;
+    boolean _state;
+
+  public:
+    SwitchButton(byte pin){
+      _pinNum=pin;
+      _stateChangeCount=0;
+      pinMode(_pinNum,INPUT_PULLUP);
+      _state=digitalRead(_pinNum);  
+    }
+
+    unsigned long getStateChangeCount(){
+       boolean nowState = digitalRead(_pinNum);
+       if (_state!=nowState){
+          if (nowState == digitalRead(_pinNum)) {
+           _state = nowState;
+           _stateChangeCount++;
+          } 
+       } 
+    return _stateChangeCount; 
+    }
+  
+};
--- button2/button2.ino
+++ button2/button2.ino
@@ -1,32 +1,7 @@
 #include "Led.h"
 #define BtnPin 2
+#include "SwitchButton.h"
 
-class SwitchButton{
-  private:
-    byte _pinNum;
-    unsigned long _stateChangeCount;
-    boolean _state;
-
-  public:
-    SwitchButton(byte pin){
-      _pinNum=pin;
-      _stateChangeCount=0;
-      pinMode(_pinNum,INPUT_PULLUP);
-      _state=digitalRead(_pinNum);  
-    }
-
-    unsigned long getStateChangeCount(){
-       boolean nowState = digitalRead(BtnPin);
-       if (_state!=nowState){
-          if (nowState == digitalRead(BtnPin)) {
-           _state = nowState;
-           _stateChangeCount++;
-          } 
-       } 
-    return _stateChangeCount; 
-    }
-  
-};
 LED redLed(13);
 SwitchButton btn(2);
 void setup() {
