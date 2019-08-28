76861e42e75797b9ea75cbcba0394eaede9a195b
Capel001 <willychang008@gmail.com>
2019-08-28 16:59:18 +0800

    Update seven_segmented.ino

--- seven_segmented/seven_segmented.ino
+++ seven_segmented/seven_segmented.ino
@@ -2,12 +2,54 @@
 //參考的網址
 //http://programmermagazine.github.io/201307/htm/article1.html
 //***********************************************************
-void setup() {
-  // put your setup code here, to run once:
+// 七段顯示器製作倒數功能 (v1)
 
+#include "SwitchButton.h"
+
+SwitchButton btn(10);
+
+byte digits[10][7] = { 
+  { 1, 1, 1, 1, 1, 1, 0 }, // = 0
+  { 0, 1, 1, 0, 0, 0, 0 }, // = 1
+  { 1, 1, 0, 1, 1, 0, 1 }, // = 2
+  { 1, 1, 1, 1, 0, 0, 1 }, // = 3
+  { 0, 1, 1, 0, 0, 1, 1 }, // = 4
+  { 1, 0, 1, 1, 0, 1, 1 }, // = 5
+  { 1, 0, 1, 1, 1, 1, 1 }, // = 6
+  { 1, 1, 1, 0, 0, 0, 0 }, // = 7
+  { 1, 1, 1, 1, 1, 1, 1 }, // = 8
+  { 1, 1, 1, 0, 0, 1, 1 } // = 9
+};
+
+
+byte mapArdiuinoPin[] = {2, 3, 4, 5, 6, 7, 8};
+
+void setup() {
+  Serial.begin(115200);
+  for (byte i = 0; i < 7; i++) {
+    pinMode(mapArdiuinoPin[i], OUTPUT);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  unsigned long BtnCount=btn.getStateChangeCount()%20;
+  unsigned long PinCount=BtnCount%2;
+  Serial.println(BtnCount);
+  Serial.println(PinCount);
+  switch(PinCount){
+    case 0:
+    displayDigit(BtnCount);
+    delay(100);
+    break;
+  }
+}
 
+
+void displayDigit(unsigned long BtnCount){
+    unsigned long PinIndex = BtnCount/2;
+    for(int i=0; i<7 ; i++){
+      bool onePinState = digits[PinIndex][i];
+      byte pinNum = mapArdiuinoPin[i];
+      digitalWrite(pinNum,onePinState);
+    }
 }
