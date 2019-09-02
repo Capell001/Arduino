1d5e61c43f415ba902fb31ecbaa6e87241857daf
Capel001 <willychang008@gmail.com>
2019-09-02 11:24:29 +0800

    ESP8266

---
+++ ESP8266/ESP8266.ino
@@ -0,0 +1,25 @@
+#include "FirebaseESP8266.h"
+#include <ESP8266WiFi.h>
+
+FirebaseData firebaseData;
+void setup()
+{
+  //連線WIFI
+  Serial.begin(115200);
+  Serial.println();
+  WiFi.begin("robert_hsu", "1234567890");
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
+  //連線firebase
+  Firebase.begin("iotcapel.firebaseio.com","v6vaSLzCienhRXC88xIrTaJgaCGF4fAKgBiwkTi7");
+}
+
+void loop() {}
