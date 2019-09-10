dd23c11fef782d315d9f7a77d5662b52fe945181
Capel001 <willychang008@gmail.com>
2019-09-10 08:56:00 +0800

    Create lm35_74CH595_4digit_seven_segmented.ino

---
+++ lm35_74CH595_4digit_seven_segmented/lm35_74CH595_4digit_seven_segmented.ino
@@ -0,0 +1,17 @@
+#define tempPin A0
+
+float tempC;
+
+
+void setup()
+{
+  Serial.begin(115200); //opens serial port, sets data rate to 9600 bps
+}
+
+void loop()
+{
+  tempC = analogRead(tempPin);           //read the value from the sensor
+  tempC = (5.0 * tempC * 100.0) / 1024.0; //convert the analog data to temperature
+  Serial.println(tempC);
+  delay(1000);
+}
